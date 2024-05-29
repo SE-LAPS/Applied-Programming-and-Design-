b5e0134eae194420b8160c0e72ba09f5dcbe63cd
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-05-29 13:47:44 +0530

    Create ISP.py

---
+++ ISP.py
@@ -0,0 +1,16 @@
+class DataLoader:
+    def load_data(self):
+        pass
+
+class DataAnalyzer:
+    def analyze_data(self):
+        pass
+
+class DataVisualizer:
+    def visualize_data(self):
+        pass
+
+class SalesDataLoader(DataLoader):
+    def load_data(self):
+        # Load sales data
+        pass
