7748c2f81cebc428b653939b9b4fa05e7b6cbcf8
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-05-29 13:47:20 +0530

    Create SRP.py

---
+++ SRP.py
@@ -0,0 +1,9 @@
+class SalesDataLoader:
+    def load_data(self, source):
+        # Code to load data from the source
+        pass
+
+class SalesDataAnalyzer:
+    def analyze_sales(self, data):
+        # Code to analyze sales data
+        pass
