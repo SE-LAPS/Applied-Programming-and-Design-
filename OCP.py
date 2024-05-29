bc433c9d369fd49e03f648756008d745ccb9ce87
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-05-29 13:47:31 +0530

    Create OCP.py

---
+++ OCP.py
@@ -0,0 +1,13 @@
+class ProductAnalyzer:
+    def analyze(self):
+        pass
+
+class PriceAnalyzer(ProductAnalyzer):
+    def analyze(self):
+        # Specific price analysis
+        pass
+
+class SalesVolumeAnalyzer(ProductAnalyzer):
+    def analyze(self):
+        # Specific sales volume analysis
+        pass
